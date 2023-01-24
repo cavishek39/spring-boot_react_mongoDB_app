f4f5013b4ad027cc335999484ad052d787f6c818
cavishek39 <cavishek39@gmail.com>
2023-01-25 00:01:48 +0530

    find movie by id(also by imdb id not the ObjectId)

--- src/main/java/dev/cavishek39/movies/MovieService.java
+++ src/main/java/dev/cavishek39/movies/MovieService.java
@@ -1,9 +1,11 @@
 package dev.cavishek39.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -12,5 +14,10 @@ public class MovieService {
     private MovieRepository movieRepository;
     public List<Movie> allMovies() {
         return movieRepository.findAll();
+    }
+
+    // Get a Movie by its ID. In some cases it might return null, so Optional
+    public Optional<Movie> movieById(String id) {
+        return movieRepository.findMovieByImdbId(id);
     }
 }
